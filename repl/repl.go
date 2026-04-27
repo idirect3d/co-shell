36162fd7a2dada2171f5210ca6ad6db9b86953e5
LiangShuang <idirect3d@hotmail.com>
2026-04-28 03:05:41 +0800

    在 .help 中补充 .plan 命令说明

--- repl/repl.go
+++ repl/repl.go
@@ -481,6 +481,7 @@ func (r *REPL) printHelp() {
 	fmt.Println(i18n.T(i18n.KeyHelpLast))
 	fmt.Println(i18n.T(i18n.KeyHelpFirst))
 	fmt.Println(i18n.T(i18n.KeyHelpImage))
+	fmt.Println(i18n.T(i18n.KeyHelpPlan))
 	fmt.Println(i18n.T(i18n.KeyHelpWizard))
 	fmt.Println(i18n.T(i18n.KeyHelpHelp))
 	fmt.Println(i18n.T(i18n.KeyHelpExit))
