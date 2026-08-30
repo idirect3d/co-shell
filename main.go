5ecfe54f7ac342c9878d4af8afc8b5e7296665e2
L.Shuang <direct3d@gmail.com>
2026-08-30 15:52:38 +0800

    FEATURE-458: Web UI 连接状态开关 + 手动重连策略 (BUILD-740)

--- main.go
+++ main.go
@@ -49,9 +49,9 @@ import (
 	"github.com/idirect3d/co-shell/workspace"
 )
 
-const version = "0.25.0"
+const version = "0.26.0"
 
-const build = "738"
+const build = "740"
 
 // cliFlags holds parsed command-line flags.
 type cliFlags struct {
